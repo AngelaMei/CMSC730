807b707d357bd69a32764a71b13d56fe4c1b045e
AngelaMei <54565258+AngelaMei@users.noreply.github.com>
2024-09-07 21:59:43 -0400

    MorseCode

--- PlatformIO/Projects/TestCode/src/main.cpp
+++ PlatformIO/Projects/TestCode/src/main.cpp
@@ -1,22 +1,45 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int ledPin = 23; // Default LED is connected to GPIO 23
 
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+const char *morseCode[] = {
+    "-----",".----","..---","...--","....-","....-", 
+    ".....","-....","--...","---..","----."
+};
+
+void morseCodeConverter(char c){
+  if (c >= '0' && c <= '9')
+  {
+    int index = c - '0';
+    for (int i = 0; morseCode[index][i] != '\0'; i++){
+      if (morseCode[index][i] == '.'){
+        digitalWrite(ledPin, HIGH);
+        delay(100);
+        Serial.print(morseCode[index][i]);
+      } else if (morseCode[index][i] == '-')
+      {
+        digitalWrite(ledPin, HIGH);
+        delay(300);
+        Serial.print(morseCode[index][i]);
+      }
+      digitalWrite(ledPin, LOW);
+      delay(100);
+    } 
+    delay(300);
+  }
+}
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (Serial.available() > 0) {
+  // Read the incoming byte
+  char incomingByte = Serial.read();
+  Serial.print("You entered: ");
+  Serial.println(incomingByte);
+  morseCodeConverter(incomingByte);
+  }
 }
