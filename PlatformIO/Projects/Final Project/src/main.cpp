b784994434c8d53fe74f149fb2b2d029af15fbf3
AngelaMei <54565258+AngelaMei@users.noreply.github.com>
2024-10-17 22:12:53 -0400

    Add Random function

--- PlatformIO/Projects/Final Project/src/main.cpp
+++ PlatformIO/Projects/Final Project/src/main.cpp
@@ -1,14 +1,20 @@
 #include <Arduino.h>
+#include <cstdlib>
 
 const int vibrator1 = 23;
 const int vibrator2 = 22;
 const int vibrator3 = 21;
+int vibrators[3];
 
 int pwmValue1 = 0;
 int pwmValue2 = 0;
 int pwmValue3 = 0;
 
 void setup() {
+  vibrators[0] = vibrator1;
+  vibrators[1] = vibrator2;
+  vibrators[2] = vibrator3;
+
   pinMode(vibrator1, OUTPUT);
   pinMode(vibrator2, OUTPUT);
   pinMode(vibrator3, OUTPUT);
@@ -16,66 +22,29 @@ void setup() {
 }
 
 void loop() {
+  int random_id = rand() % 3;
+  analogWrite(vibrators[random_id], 10000);
+  Serial.print(random_id);
+  delay(200);
+  analogWrite(vibrators[random_id], 0);
+  delay(1000);
 
-  analogWrite(vibrator1, 3);
-  Serial.print("3");
-  delay(1000);
+  /*
+  analogWrite(vibrator1, 10000);
+  delay(200);
   analogWrite(vibrator1, 0);
   delay(1000);
-  analogWrite(vibrator1, 20);
-  Serial.print("20");
-  delay(1000);
-  analogWrite(vibrator1, 0);
-  delay(1000);
-  analogWrite(vibrator1, 25);
-  Serial.print("25");
-  delay(1000);
-  analogWrite(vibrator1, 0);
-  delay(1000);
-  analogWrite(vibrator1, 45);
-  Serial.print("45");
-  delay(1000);
-  analogWrite(vibrator1, 0);
-  delay(1000);
-  analogWrite(vibrator1, 60);
-  Serial.print("60");
-  delay(1000);
-  analogWrite(vibrator1, 0);
-  delay(1000);
-  analogWrite(vibrator2, 100);
-  Serial.print("100");
-  delay(1000);
+
+  analogWrite(vibrator2, 10000);
+  delay(200);
   analogWrite(vibrator2, 0);
   delay(1000);
-  analogWrite(vibrator3, 500);
-  Serial.print("500");
-  delay(1000);
-  analogWrite(vibrator3, 0);
-  delay(1000);
-  analogWrite(vibrator3, 1000);
-  Serial.print("1000");
-  delay(1000);
-  analogWrite(vibrator3, 0);
-  delay(1000);
-  analogWrite(vibrator3, 1500);
-  Serial.print("1500");
-  delay(1000);
-  analogWrite(vibrator3, 0);
-  delay(1000);
-  analogWrite(vibrator3, 2000);
-  Serial.print("2000");
-  delay(1000);
-  analogWrite(vibrator3, 0);
-  delay(1000);
-  analogWrite(vibrator3, 3000);
-  Serial.print("3000");
-  delay(1000);
-  analogWrite(vibrator3, 0);
-  delay(1000);
 
-
-
-
+  analogWrite(vibrator3, 10000);
+  delay(200);
+  analogWrite(vibrator3, 0);
+  delay(1000);
+  */
 
   // for (int i = 0; i < 1000; i+=100) {
   //   pwmValue1 = i;
